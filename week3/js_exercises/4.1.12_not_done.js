9023f5db1c941056a557a96f221ff6cc008df8ab
TKonstantinova <tetiana.konstantinova@powercoders.org>
2024-09-30 21:12:46 +0200

    Add js exercises

---
+++ week3/js_exercises/4.1.12_not_done.js
@@ -0,0 +1,6 @@
+/*The intended behavior is to increment by one but why is this function not working? Can you fix this? (try to solve this on paper first and then debug the code)*/
+function incrementItems (arr) {
+    for (let i = 0; i < array . length ; i++)
+        arr [i] === arr[i] + 1
+        return array
+}
