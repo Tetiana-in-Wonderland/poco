c4af0773d445d255e783f0cfd9843d7acdc2921c
TKonstantinova <tetiana.konstantinova@powercoders.org>
2024-10-21 17:17:49 +0200

    Add API exercise

--- week6/script.js
+++ week6/script.js
@@ -1,6 +1,6 @@
 function onsubmit(event) {
     event.preventDefault();
-
+    debugger
     const fromCity = document.getElementById("fromCity").value;
     const toCity = document.getElementById("toCity").value;
     const apiUrl = `https://transport.opendata.ch/v1/connections?from=${fromCity}&to=${toCity}`;
